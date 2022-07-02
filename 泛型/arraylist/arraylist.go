cbe2855fe8a415121c99e29489be9cdf75b2999d
machengling <861021854@qq.com>
2022-07-02 22:36:21 +0800

    add arraylist

--- 泛型/arraylist/arraylist.go
+++ 泛型/arraylist/arraylist.go
@@ -1,1 +1,48 @@
-package arraylist
+package main
+
+import "sort"
+
+type Number interface {
+	int | int32 | int64 | float64 | float32
+}
+
+type arrayList[K Number] struct {
+	data  *[]K
+	sortL bool
+}
+
+func NewArrayList[K Number]() arrayList[K] {
+	list := arrayList[K]{}
+	list.data = &[]K{}
+	return list
+}
+
+func (list *arrayList[K]) Less(i, j int) bool {
+	if list.sortL {
+		return (*list.data)[i] < (*list.data)[j]
+	}
+	return (*list.data)[i] > (*list.data)[j]
+}
+
+func (list *arrayList[K]) Swap(i, j int) {
+	(*list.data)[i], (*list.data)[j] = (*list.data)[j], (*list.data)[i]
+
+}
+
+func (list *arrayList[K]) Len() int {
+	return len(*list.data)
+}
+
+func (list *arrayList[K]) add(item K) {
+	*list.data = append(*list.data, item)
+}
+
+func (list *arrayList[K]) sortLess() {
+	list.sortL = true
+	sort.Sort(list)
+}
+
+func (list *arrayList[K]) sortBig() {
+	list.sortL = false
+	sort.Sort(list)
+}
--- 泛型/arraylist/arraylist_test.go
+++ 泛型/arraylist/arraylist_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestArrayList(t *testing.T) {
+
+	list := NewArrayList[int]()
+
+	list.add(10)
+	list.add(9)
+	list.add(3)
+	list.add(5)
+	list.add(7)
+	list.add(6)
+	list.add(1)
+	list.sortLess()
+	fmt.Println(*list.data)
+	list.sortBig()
+	fmt.Println(*list.data)
+}
